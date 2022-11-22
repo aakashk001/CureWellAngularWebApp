f12bd68bac31e5edce62fe03a6df139b84a4d2c1
Aakash kondal <aakashkondal365@outlook.com>
2022-11-22 18:58:28 +0530

    Working on UserHome Component

--- src/app/user/userhome/userhome.component.ts
+++ src/app/user/userhome/userhome.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-
-
+import { UserhomeService } from './userhome.service';
+ 
 @Component({
   selector: 'app-userhome',
   templateUrl: './userhome.component.html',
@@ -10,19 +10,28 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 
 export class UserhomeComponent implements OnInit {
 
-  
-
-  constructor() {}
+  imgUrl!:string;
+  coachArray!:any[]
+  showcoach!:boolean
+  constructor(private service:UserhomeService) {}
   
 
   ngOnInit() {
-    
+    this.allcoaches();
   }
 
-  
+  allcoaches(){
+    this.service.allcoaches().subscribe({
+      next: (data) => {if(data){
+        this.coachArray = data;
+        this.showcoach = true;
+        if(this.coachArray[0].gender = "M"){
+          this.imgUrl = '../../../assets/Images/male.png'
+        }
+        else if(this.coachArray[0].gender = "F"){
+          this.imgUrl = '../../../assets/Imsges/female.png'
+        }
+      }}
+    })
+  }
 }
-
-function dateCalculator(fc: FormControl) {
- 
-}
-
--- src/app/user/userhome/userhome.service.ts
+++ src/app/user/userhome/userhome.service.ts
@@ -1,10 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserhomeService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
+  allcoaches():Observable<any[]>{
+    return this.http.get<any[]>('http://localhost:3000/coaches').pipe(tap(data=> console.log(data)))
+  }
 }
